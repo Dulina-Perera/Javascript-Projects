4326d666e60fbce972a46a58b4c8a786c64201cc
Dulina-Perera <dulina.perera@outlook.com>
2024-09-14 20:24:57 +0530

    Learn-Jest: Learnt about `toEqual` and `toStrictEqual` matchers.

--- Learn-Jest/sum.test.js
+++ Learn-Jest/sum.test.js
@@ -3,3 +3,9 @@ import sum from "./sum";
 test('check if 1 + 2 = 3', () => {
 	expect(sum(1, 2)).toBe(3);
 });
+
+test('object assignment', () => {
+	const data = { one: 1 };
+	data['two'] = 2;
+	expect(data).toStrictEqual({ one: 1, two: 2 });
+});
